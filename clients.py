9898b1d206fec290716261cb15ce75b9ef58c214
zipsonic <rick@hail3.com>
2024-11-17 23:03:49 -0800

    Getting closer to a functional program

--- clients.py
+++ clients.py
@@ -1,27 +1,36 @@
 import uuid
 import re
 
+class Address:
+    street1: str
+    street2: str
+    city: str
+    state: str
+    zip: str
+
 class Client:
     def __init__(self, first_name, last_name, email=None, phone=None, address=None):
         """
         Initializes a Client object with the given attributes.
         """
-        self.__client_id = uuid.uuid4()
-        self.first_name = first_name
-        self.last_name = last_name
+        self.__client_id: uuid.UUID = uuid.uuid4()
+        self.first_name: str = first_name
+        self.last_name: str = last_name
         if email:
             self.update_email(email)
         if phone:
             self.update_phone(phone)
+        # if address:
+        #     self.address: Address = address
         self.address = address
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"Client({self.__client_id}): {self.last_name}, {self.first_name} <{self.email}>"
     
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Client: {self.last_name}, {self.first_name}\n<{self.email}> {self.phone}"
 
-    def get_contact_info(self):
+    def get_contact_info(self) -> str:
         """
         Returns a formatted string of the client's contact information.
         """
@@ -34,7 +43,7 @@ class Client:
             contact_info += f"\nAddress: {self.address}"
         return contact_info
 
-    def update_email(self, new_email):
+    def update_email(self, new_email) -> None:
         """
         Updates the client's email address. Checks for address validity before updating.
 
@@ -42,7 +51,7 @@ class Client:
         """
         valid: re.Match[str] | None = re.match(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', new_email)
         if valid:
-            self.email = new_email
+            self.email: str = new_email
 
     def update_phone(self, new_phone):
         """
@@ -51,4 +60,4 @@ class Client:
         :param new_phone: New phone number (str)
         """
         if len(new_phone) == 10:
-            self.phone = f"({new_phone[:3]}) {new_phone[3:6]}-{new_phone[6:]}"
+            self.phone: str = f"({new_phone[:3]}) {new_phone[3:6]}-{new_phone[6:]}"
--- main.py
+++ main.py
@@ -2,39 +2,76 @@ import curses
 from curses import panel
 from clients import Client
 
-def enter_client() -> None:
-    entrywin = curses.newwin(28,60,1,60)
+def enter_client(clientlist: list[Client]) -> None:
+    curses.curs_set(1)
+    entrywin: curses._CursesWindow = curses.newwin(28,60,1,60)
     entrywin.bkgd(' ',curses.color_pair(2))
-    entrypanel = panel.new_panel(entrywin)
+    entrypanel: panel._Curses_Panel = panel.new_panel(entrywin)
     entrypanel.show()
     entrywin.box()
     entrywin.refresh()
     entrywin.getch()
     entrypanel.hide()
+
+    curses.curs_set(0)
     entrywin.refresh()
 
+    
+def display_client_list(clientlist: list[Client]) -> 'curses._CursesWindow':
+    clientdatawin: curses._CursesWindow = curses.newwin(30,60,0,60)
+    clientdatawin.bkgd(' ',curses.color_pair(1))
+    clientdatapanel: panel._Curses_Panel = panel.new_panel(clientdatawin)
+    clientdatapanel.show()
+    clientdatawin.box()
+
+    for i, client in enumerate(iterable=clientlist):
+        for j, line in enumerate(iterable=client.get_contact_info().split("\n")):
+            clientdatawin.addstr((j+1)+(i*3),1,line)
+    
+    return clientdatawin
 
 def main(stdscr: 'curses._CursesWindow') -> None:
     curses.curs_set(0)
     stdscr.clear()
 
+    #initialize Client List
+    clientlist: list[Client] = []
+
+    # Sample Data ---- REMOVE ME
+    clientlist.append(Client(
+        "Johnathan",
+        "Dough",
+        "jd@example.net",
+        "2138675309"
+    ))
+    clientlist.append(Client(
+        "Jane",
+        "Doe",
+        "jdoe@example.net",
+        "3108675309"
+    ))
+    # End Sample Data ----- REMOVE ME
+
     #Initialize color pairs
     curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLUE)
     curses.init_pair(2,curses.COLOR_WHITE,curses.COLOR_GREEN)
-    
+
     stdscr.bkgd(' ', curses.color_pair(1))
     stdscr.box()
     stdscr.addstr(curses.LINES-1,5,"[F2 Enter Client]")
 
+    clientdatawin: curses._CursesWindow = display_client_list(clientlist=clientlist)
+
     while True:
 
         stdscr.refresh()
+        clientdatawin.refresh()
 
-        key = stdscr.getch()
+        key: int = stdscr.getch()
 
         if key == curses.KEY_F2:
             key = key
-            enter_client()
+            enter_client(clientlist)
         
 
 
